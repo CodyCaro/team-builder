f975b3bd91194afee9df2a6576a93e0d2727e2cb
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-08-13 18:59:12 -0700

    Finished creating the form and made it save and show the values inputed

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TeamMember from "./components/TeamMember";
+import Form from "./components/Form";
+import data from "./data";
 
 function App() {
+  const [teamMember, setTeamMember] = useState(data);
+
+  const addNewMember = member => {
+    console.log(member);
+    setTeamMember([...teamMember, member]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form addNewMember={addNewMember} />
+      <TeamMember memberList={teamMember} />
     </div>
   );
 }
